0eab46b9b22408dc89721dcae21dd54d12620a06
dima-u <dima-u@inbox.ru>
2022-01-12 15:19:06 +0300

    fix

--- Sources/CBlurHash/include/CBlurHash.h
+++ Sources/CBlurHash/include/CBlurHash.h
@@ -8,6 +8,9 @@
 #ifndef CBlurHash
 #define CBlurHash
 
+#include <stdlib.h>
+#include <stdint.h>
+
 uint8_t * decode(const char * blurhash, int width, int height, int punch, int nChannels);
 
 const char *blurHashForPixels(int xComponents, int yComponents, int width, int height, uint8_t *rgb, size_t bytesPerRow);
