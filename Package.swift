42b2ce4d0e627681577223b3c14076553960a6ac
dima-u <dima-u@inbox.ru>
2022-01-12 15:56:25 +0300

    try several targets

--- Package.swift
+++ Package.swift
@@ -5,17 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "CBlurHash",
+    platforms: [
+        .iOS(.v13)
+        ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CBlurHash",
-            targets: ["blurhash"]),
+            targets: ["blurhash", "CBlurHash"]),
     ],
     targets: [
-      //  .systemLibrary(name: "CBlurHash"),
         .target(
           name: "blurhash",
           dependencies: []
+        ),
+        .target(
+          name: "CBlurHash",
+          dependencies: ["blurhash"]
         )
     ]
 )
---
+++ Sources/CBlurHash/CBlurHash.swift
@@ -0,0 +1,74 @@
+//  Image+Blurhash.swift
+//
+//  Created by dmitriy Uyanov on 12.01.2022.
+//  dima-u@inbox.ru
+//
+
+import blurhash
+
+#if canImport(UIKit)
+
+import UIKit
+
+extension UIImage {
+    // decode
+    convenience init?(blurHash: String, size: CGSize) {
+        guard let pixelBuffer = decode(blurHash, Int32(size.width), Int32(size.height), 1, 3) else {
+            return nil
+        }
+
+        let bytes = Data(bytesNoCopy: UnsafeMutableRawPointer(pixelBuffer),
+                         count: Int(size.width * size.height) * 3,
+                         deallocator: .custom { pppp, _ in
+                             free(pppp)
+                         })
+
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
+        let bitsPerComponent = 8
+        let bitsPerPixel = 24
+
+        let providerRef = CGDataProvider(data: bytes as CFData)
+
+        if let cgim = CGImage(
+            width: Int(size.width),
+            height: Int(size.height),
+            bitsPerComponent: bitsPerComponent,
+            bitsPerPixel: bitsPerPixel,
+            bytesPerRow: Int(size.width) * 3,
+            space: rgbColorSpace,
+            bitmapInfo: bitmapInfo,
+            provider: providerRef!,
+            decode: nil,
+            shouldInterpolate: true,
+            intent: .defaultIntent
+        ) {
+            self.init(cgImage: cgim)
+        }
+
+        return nil
+    }
+
+    // encode
+    func blurHash(xComponents: Int, yComponents: Int) -> String? {
+        guard let cgImage = cgImage,
+              let data = cgImage.dataProvider?.data,
+              let bytes = CFDataGetBytePtr(data)
+        else {
+            return nil
+        }
+
+        guard cgImage.colorSpace?.model == .rgb else {
+            return nil
+        }
+
+        let pointer = UnsafeMutablePointer(mutating: bytes)
+
+        if let hash = blurHashForPixels(Int32(xComponents), Int32(yComponents), Int32(size.width), Int32(size.height), pointer, cgImage.bytesPerRow) {
+            return String(cString: hash)
+        }
+        return nil
+    }
+}
+
+#endif
